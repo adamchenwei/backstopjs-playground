de70d4ca895fb91b38b811a3fe556a1d52474a50
Adam Chen Wei <achenwei@fll1006558l.chewy.local>
2018-10-29 11:43:01 -0400

    test(backstopjs): inital setup with broken tests

---
+++ backstopjs-instance/backstop_data/html_report/config.js
@@ -0,0 +1,40 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_0_phone.png",
+        "test": "../bitmaps_test/20181029-112856/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_0_phone.png",
+        "selector": ".vue-logo-img",
+        "fileName": "backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_0_phone.png",
+        "label": "Vue Logo Image Must Show At All Time",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8081",
+        "referenceUrl": "http://localhost:8082",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "error": "Reference file not found /Users/achenwei/www/lab/backstopjs-playground/backstopjs-instance/backstop_data/bitmaps_reference/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_1_tablet.png",
+        "test": "../bitmaps_test/20181029-112856/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_1_tablet.png",
+        "selector": ".vue-logo-img",
+        "fileName": "backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_1_tablet.png",
+        "label": "Vue Logo Image Must Show At All Time",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8081",
+        "referenceUrl": "http://localhost:8082",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "error": "Reference file not found /Users/achenwei/www/lab/backstopjs-playground/backstopjs-instance/backstop_data/bitmaps_reference/backstop_default_Vue_Logo_Image_Must_Show_At_All_Time_0_vue-logo-img_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
